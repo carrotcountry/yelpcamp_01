a4183afb3b78160d6a39f337eedbcc40d641b994
carrotcountry <jaeyoon321@naver.com>
2023-02-25 19:42:08 +0900

    no message

--- yelpCamp/models/campground.js
+++ yelpCamp/models/campground.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //단축키 역할 추후 mongoose.Schema의 사용 빈도가 잦으므로
 
-// const campinggroundSchema = new mongoose.Schema({ 
-//     name: 'string', 
-//     size: 'string'
-// });
 
 // 스키마 만들 때 new ~ 부분
 
